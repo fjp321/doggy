d801890c1b165ebc87c94e93943262a0db5917d2
fjp321 <potter.finley@gmail.com>
2021-08-02 11:50:54 -0400

    editted init db to init a xml file

---
+++ init-db.py
@@ -0,0 +1,8 @@
+import sys
+import numpy as np
+import scipy as sp
+import pandas as pd
+import lxml
+db = open('main-db.xml', 'w')
+db.write('<?xml version=\"1.0\" encoding=\"utf-8\"?>\n')
+db.close()
