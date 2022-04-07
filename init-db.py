7b52bc83da8d5fadc2c41aad3c5c6f3a5f2944e5
fjp321 <potter.finley@gmail.com>
2022-04-06 20:39:17 -0400

    added init py files for ingredient dictionary and type list

--- init-db.py
+++ init-db.py
@@ -2,14 +2,6 @@ import sys
 import json
 import pandas as pd
 
-# initialize hash table of usable ingredients, size 5000
-
-# write hash table to usable_ingredients as binary
-
-# initialize list of strings of acceptable types
-
-# write list of strings into acceptable_types as binary
-
 # initialize dataframe with name, type, ingredients, and recipe
 df = pd.DataFrame(columns = ["name", "type", "ingredients", "recipe"])
 
