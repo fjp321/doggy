590677590f36370209a5cbdfe4e1c666414d4a22
Spencer-Potter <potter.spencer@gmail.com>
2022-04-13 23:58:16 -0400

    made import work for ubuntu

--- test/test_init_db.py
+++ test/test_init_db.py
@@ -1,4 +1,11 @@
 import unittest
+# (for ubuntu users, change import for doggy)
+# appending path to src/doggy.py
+# code needed below
+# import sys
+# sys.path.append("../src")
+# import doggy.py as doggy
+
 import src.doggy as doggy
 
 
