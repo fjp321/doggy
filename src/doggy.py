ac369b9730d28475ea98056b975fead429482c4f
fjp321 <potter.finley@gmail.com>
2022-04-14 10:04:00 -0400

    updated functions with file location parameter with default value

--- src/doggy.py
+++ src/doggy.py
@@ -7,44 +7,44 @@ no_error_code = 0
 input_error_code = -1
 already_exists_error_code = -2
 
-database_location = "data/database.json"
-ingredients_location = "data/ingredients.json"
-cuisines_location = "data/cuisines.csv"
+default_database_location = "database.json"
+default_ingredients_location = "data/ingredients.json"
+default_cuisines_location = "data/cuisines.csv"
 
 
 # creates pandas dataframe with needed columns and save dataframe to json
-def init_db():
+def init_db(file_location=default_database_location):
     # initialize dataframe with name, cuisine, ingredients, and recipe
     database_df = pd.DataFrame(columns=["name", "cuisine", "ingredients", "image"])
 
     # write dataframe to database to data.json
-    database_file = open(database_location, 'w')
+    database_file = open(file_location, 'w')
     database_file.write(database_df.to_json())
     database_file.close()
 
 
 # saves files for cuisines as csv
-def init_cuisines():
+def init_cuisines(file_location=default_cuisines_location):
     # touch empty file for cuisines, save as csv
-    cuisines_file = open(cuisines_location, 'w')
+    cuisines_file = open(file_location, 'w')
     cuisines_file.close()
 
 
 # saves ingredients dictionary to json file
-def init_ingredients():
+def init_ingredients(file_location=default_ingredients_location):
     # touch empty file for ingredients, save as json
-    ingredients_file = open(ingredients_location, 'w')
+    ingredients_file = open(file_location, 'w')
     ingredients_file.close()
 
 
 # helper function to print recipes file
-def print_database():
-    return pd.read_json(database_location).to_string()
+def print_database(file_location=default_database_location):
+    return pd.read_json(file_location).to_string()
 
 
 # helper function to print cuisines list
-def print_cuisines():
-    cuisines_file = open(cuisines_location, 'r')
+def print_cuisines(file_location=default_cuisines_location):
+    cuisines_file = open(file_location, 'r')
     csv_reader = csv.reader(cuisines_file, delimiter=' ')
     for row in csv_reader:
         print(row + ',')
@@ -52,8 +52,8 @@ def print_cuisines():
 
 
 # helper function to print ingredients dictionary
-def print_ingredients():
-    ingredients_file = open(ingredients_location, 'r')
+def print_ingredients(file_location=default_ingredients_location):
+    ingredients_file = open(file_location, 'r')
     print(json.load(ingredients_file))
 
 
@@ -62,11 +62,11 @@ def contain_name(input_name):
     print("placeholder")
 
 
-def contain_cuisine(input_cuisine):
+def contain_cuisine(input_cuisine, file_location=default_cuisines_location):
     if not (isinstance(input_cuisine, str)):
         print("input is not a string")
         return input_error_code
-    cuisines_file = open(cuisines_location, 'r')
+    cuisines_file = open(file_location, 'r')
     csv_reader = csv.reader(cuisines_file, delimiter=' ')
 
 
@@ -81,14 +81,14 @@ def add_recipe(input_cuisine, input_name, input_image, input_ingredients):
 
 # opens csv and reads into list, then appends input string cuisine to list and saves as csv
 # will raise exception if input is not string
-def add_cuisine(input_cuisine):
+def add_cuisine(input_cuisine,file_location=default_cuisines_location):
     if not isinstance(input_cuisine, str):
         print("input is not string")
         return input_error_code
     if (contain_cuisine(input_cuisine)):
         print("input already in cuisines.csv")
         return already_exists_error_code
-    cuisines_file = open(cuisines_location, 'a')
+    cuisines_file = open(file_location, 'a')
     cuisines_file.write(input_cuisine)
     cuisines_file.close()
     return no_error_code
@@ -111,8 +111,8 @@ def del_ingredient():
     print("placeholder")
 
 
-def get_database():
-    return pd.read_json(database_location)
+def get_database(file_location=default_database_location):
+    return pd.read_json(file_location)
 
 
 def __init__(self):
