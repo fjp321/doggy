b257767bfa35ed89513d1ca62901b2d38e202f4e
fjp321 <potter.finley@gmail.com>
2022-04-25 00:34:49 -0400

    updated for doxygen workflow plus updated README.md

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
-## Description
+# Doggy Recipes Generation
 
-Recipe object creator, adding recipes to a sql database. 
+## What is Doggy
 
-## Features
+Doggy is a python library for the creation and management of recipe databases. It also has functionality for generating randomized grocery lists for a specified number of recipes. 
 
-* Ingredient database, with associated cost and tags for type and theme of meal
-* Grocery list genereation
-    * Can specify when to have particular meal
-    * Can request theme for meals, i.e. latin american, korean, etc.
+## Usage
 
-## Thanks for visiting this page
+To use this library, simply add doggy.py to where ever your python path points. This library will be added to pypi, and when this is implemented, We will update the README. 
 
-Also
+## [Documentation]()
 
-## Have a great day
+## Design Choices
+
+### Why SQL instead of json
+
+The reason for sql, and  by extension python, is for the [sqlite](https://en.wikipedia.org/wiki/SQLite) python library, and a database is the better format for a scalability. The goal of Doggy is to manage potentially hundreds of recipes and ingredients, so scalability is incredibly important. 
+
+### Why no GUI
+
+There is no gui, but there is a command line helper script, planet. The reason this is prioritized over a gui is so that adding this helper script to a [cron job](https://en.wikipedia.org/wiki/Cron)is as simple as possible. If you want a gui, please look into making one yourself! Consider using [pyqt](https://en.wikipedia.org/wiki/PyQt) If you run into any issues regarding the doggy functions, please feel free to open an issue.
+
+## Resources
+
+SQLite: https://en.wikipedia.org/wiki/SQLite
+Cron: https://en.wikipedia.org/wiki/Cron
+Pyqt: https://en.wikipedia.org/wiki/PyQt
+
+## [Planned Updates]()
 
 ## Contributors
 fjp321
--- src/doggy.py
+++ src/doggy.py
@@ -1,20 +1,56 @@
+##
+# @mainpage Doggy Wheel Project
+# 
+# @section descript_main Description
+# Python wheel file for database interaction and initialization for the purpose for recipe management
+
+## 
+# @file doggy.py
+#
+# @brief Wheel file containing functions for database interaction
+# 
+# @section description_doggy Description
+# Wheel file containing functions for database interaction
+# 
+# @section libraries_main Libraries/Modules
+# - sys standard library
+#   - for access to file io functions
+# - json standard library
+#   - for access to json conversion functions
+# - csv standard library
+#   - for csv reader functions
+# - pandas dataframe functions
+#   - for interaction for larger databases
+#
+
+# Imports
 import sys
 import json
 import csv
 import pandas as pd
 
+# Global Variables
+## return code for success
 no_error_code = 0
+## return code for invalid input variable types
 input_error_code = -1
+## error code for entries already existing in database
 already_exists_error_code = -2
+## error code for entries that do not exist in the database
 does_not_exist_error_code = -3
-
+## default file location for database
 default_database_location = "database.json"
+## default file location for ingredients database
 default_ingredients_location = "ingredients.json"
+## default file location for cuisines database
 default_cuisines_location = "cuisines.csv"
 
 
-# creates pandas dataframe with needed columns to represent database of recipes and save dataframe to json
+# Functions
 def init_db(file_location=default_database_location):
+    """! creates pandas dataframe with needed columns to represent database of recipes and save dataframe to json
+    @param file_location    the file location the database will be saved to
+    """
     # initialize dataframe with name, cuisine, ingredients, and recipe
     database_df = pd.DataFrame(columns=["name", "cuisine", "ingredients", "image"])
 
@@ -24,27 +60,35 @@ def init_db(file_location=default_database_location):
     database_file.close()
 
 
-# saves cuisines csv to initial loaction
 def init_cuisines(file_location=default_cuisines_location):
-    # touch empty file for cuisines, save as csv
+    """! saves cuisines csv to initial location
+    @param file_location    the file location the cuisines database will be saved to
+    """
     cuisines_file = open(file_location, 'w')
     cuisines_file.close()
 
 
-# saves ingredients dictionary to default location
 def init_ingredients(file_location=default_ingredients_location):
-    # touch empty file for ingredients, save as json
+    """! saves ingredients dictionary to initial location
+    @param file_location    the file location the ingredients database will be saved to
+    """
     ingredients_file = open(file_location, 'w')
     ingredients_file.close()
 
 
-# helper function to return recipes database as string
-def print_database(file_location=default_database_location):
+def get_database(file_location=default_database_location):
+    """! helper function to return recipes database as string
+    @param file_location    the file location the function will access to read
+    @return string database
+    """
     return pd.read_json(file_location).to_string()
 
 
-# helper function to return cuisines list
 def get_cuisines(file_location=default_cuisines_location):
+    """! Helper function to return cuisines list
+    @param file_location    the file location the function will access to read
+    @return list of strings representing all available cuisines
+    """
     cuisines_file = open(file_location, 'r')
     csv_reader = csv.reader(cuisines_file, delimiter=',')
     cuisine_list = list(csv_reader)
@@ -54,8 +98,11 @@ def get_cuisines(file_location=default_cuisines_location):
     return cuisine_list
 
 
-# helper function to return ingredients dictionary
 def get_ingredients(file_location=default_ingredients_location):
+    """! Helper function to return ingredients dictionary
+    @param file_location    the file location the function will access to read
+    @return dictionary of string keys and string values for ingredient name and measurement respectively
+    """
     ingredients_file = open(file_location, 'r')
     # added try catch for error handling on empty json file
     try:
@@ -66,13 +113,21 @@ def get_ingredients(file_location=default_ingredients_location):
     return ingredients_dict
 
 
-# helper function to check if recipe name is in recipe df
 def contain_name(input_name):
+    """! Helper function that will check if a recipe name already exists in the database. 
+    @param input_name   string name that will be checked for
+    @param file_location    the file location that will be accessed for read
+    @return will return 0 on success
+    """
     print("placeholder")
 
 
-# helper function that checks if cuisine is already saved in cuisine list, returns bool
 def contain_cuisine(input_cuisine, file_location=default_cuisines_location):
+    """! Helper function that checks if cuisine is already saved in cuisine list
+    @param input_cuisine    string that will be checked for in the ingredient database
+    @param file_location    the file location that will be accessed for read
+    @return true if input_cuisine is in list false otherwise 
+    """
     if not (isinstance(input_cuisine, str)):
         print("input is not a string")
         return input_error_code # maybe change to false for this function
@@ -80,19 +135,32 @@ def contain_cuisine(input_cuisine, file_location=default_cuisines_location):
     return input_cuisine in cuisine_list
 
 
-# helper function that checks if ingredient is in ingredients dictionary keys list
 def contain_ingredient(input_ingredient, file_location=default_ingredients_location):
+    """! Helper function that checks if ingredient is in ingredients dictionary keys list
+    @param input_ingredient string that will be checked for in ingredients dictionary key value
+    @return true if input_ingredient is in dictionary otherwise false
+    """
     ingredients_dict = get_ingredients(file_location=file_location)
     return input_ingredient in ingredients_dict.keys()
 
 
-# opens json recipes file and reads into df, add recipe based on inputs string cuisine, string dictionary ingredients, and string list steps, and saves df to json file
 def add_recipe(input_cuisine, input_name, input_image, input_ingredients):
+    """! Opens json recipes file and reads into df, add recipe based on inputs string cuisine, string dictionary ingredients, and string list steps, and saves df to json file
+    @param input_cuisine    string representing cuisine of the recipe
+    @param input_name   string name representing the name of the recipe
+    @param input_image  file location for the image of the recipe
+    @param input_ingredients    dictionary of string keys and float values, representing 
+    @return will return 0 on success
+    """
     print("placeholder")
 
 
-# opens csv and reads into list, then appends input string cuisine to list and saves as csv
 def add_cuisine(input_cuisine,file_location=default_cuisines_location):
+    """! Opens csv and reads into list, then appends input string cuisine to list and saves as csv
+    @param input_cuisine    string representing the input cuisine to be added to the database
+    @param file_location    file location of the database that the changes will be written to
+    @return will return 0 on success
+    """
     if not isinstance(input_cuisine, str):
         print("input is not string")
         return input_error_code
@@ -109,8 +177,13 @@ def add_cuisine(input_cuisine,file_location=default_cuisines_location):
     return no_error_code
 
 
-# open ingredients json and reads into dictionary, add elements based on inputs string ingredient, string measurement, then saves to json file
 def add_ingredient(input_ingredient, input_measurement, file_location=default_ingredients_location):
+    """! Open ingredients json and reads into dictionary, add elements based on inputs string ingredient, string measurement, then saves to json file
+    @param input_ingredient string representing the ingredient 
+    @param input_measurement    string representing the unit of measurement for the ingredient
+    @param file_location    file location of the database that the changes will be written to
+    @return will return 0 on success
+    """
     if not isinstance(input_ingredient,str) or not isinstance(input_measurement,str):
         print("input is not string")
         return input_error_code
@@ -127,12 +200,21 @@ def add_ingredient(input_ingredient, input_measurement, file_location=default_in
     
 
 
-def del_recipe():
+def del_recipe(input_recipe, file_location=default_database_location):
+    """! Open recipes database, and removes recipe entry based on recipe input string
+    @param input_recipe string representing the recipe to be removed
+    @param file_location    file location of the database where changes will be written to
+    @returns will return 0 on success
+    """
     print("placeholder")
 
 
-# opens cuisine list, checks if input is in list, then removes it and rewrites list to csv
 def del_cuisine(input_cuisine, file_location=default_cuisines_location):
+    """! Opens cuisine list, checks if input is in list, then removes it and rewrites list to csv
+    @param input_cuisine string representing the cusine to be removed from the database
+    @param file_location file location of the database where changes will be written to
+    @return will return 0 on success
+    """
     if not isinstance(input_cuisine,str):
         print("input is not string")
         return input_error_code
@@ -151,8 +233,12 @@ def del_cuisine(input_cuisine, file_location=default_cuisines_location):
     return no_error_code
 
 
-# opens ingredients dictionary, checks if input is in dictionary, then removes it and rewrites json
 def del_ingredient(input_ingredient, file_location=default_ingredients_location):
+    """! Opens ingredients dictionary, checks if input is in dictionary, then removes it and rewrites json
+    @param input_ingredient string that represents teh input ingredient to be deleted
+    @param file_location    file location that represents the database that will be updated
+    @return will return 0 on success
+    """
     if not isinstance(input_ingredient,str):
         print("input is not string")
         return input_error_code
@@ -168,6 +254,12 @@ def del_ingredient(input_ingredient, file_location=default_ingredients_location)
     return no_error_code
 
 def update_ingredient(input_ingredient, input_measurement, file_location=default_ingredients_location):
+    """! Helper function to update input ingredient with new measurement, or add new ingredient with specified measurement if ingredient does not exist
+    @param input_ingredient string representing ingredient that will updated
+    @param input_measurement    string representing measurement associated with ingredient
+    @param file_location    file representing the database that will be updated
+    @return will return 0 on success
+    """
     if not isinstance(input_ingredient,str):
         print("input is not string")
         return input_error_code
@@ -183,10 +275,9 @@ def update_ingredient(input_ingredient, input_measurement, file_location=default
 
 
 def get_database(file_location=default_database_location):
+    """! Helper function that will return the database as a dataframe
+    @param file_location location of the database that will be accessed and returned
+    @return dataframe that represents the recipe database
+    """
     return pd.read_json(file_location)
 
-
-def __init__(self):
-    init_db()
-    init_cuisines()
-    init_ingredients()
